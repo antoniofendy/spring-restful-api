0e9a2b1ee8421adfb052e6ce024a3d9997c48007
antoniofendy <layfenfen@gmail.com>
2023-07-19 21:06:22 +0700

    Add swagger and users entity

---
+++ src/main/java/com/fendy/restful/api/entity/User.java
@@ -0,0 +1,30 @@
+package com.fendy.restful.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String token;
+
+    @Column(name = "token_expired_at")
+    private Long tokenExpiredAt;
+}
