a3d60488b6b88d282019c556ee406f5568d4c72b
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-20 10:59:04 +0700

    Add contact entity with its relationship

---
+++ src/main/java/com/fendy/restful/api/entity/Contact.java
@@ -0,0 +1,33 @@
+package com.fendy.restful.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    private String id;
+
+    @Column(name = "user_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String phone;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+
+}
