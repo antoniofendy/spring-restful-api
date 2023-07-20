66768009127d3933eb8d75678a6f5f939ee5339c
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-20 14:06:14 +0700

    Create UserController.java

---
+++ src/main/java/com/fendy/restful/api/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.fendy.restful.api.controller;
+
+import com.fendy.restful.api.model.RegisterUserRequest;
+import com.fendy.restful.api.model.WebResponse;
+import com.fendy.restful.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(
+            path = "/api/users",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> register(@RequestBody RegisterUserRequest registerUserRequest) {
+        userService.register(registerUserRequest);
+        return WebResponse.<String>builder().data("OK").build();
+    }
+
+}
