79747c0790a9cb3e240b93dc774b95707938178b
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-20 14:02:26 +0700

    Create UserRepository.java

---
+++ src/main/java/com/fendy/restful/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fendy.restful.api.repository;
+
+import com.fendy.restful.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
