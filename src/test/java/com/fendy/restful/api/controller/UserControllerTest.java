da74a5c98cb039bc3e24155f5bb4643499c5368f
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-20 15:00:15 +0700

    Update UserControllerTest.java

--- src/test/java/com/fendy/restful/api/controller/UserControllerTest.java
+++ src/test/java/com/fendy/restful/api/controller/UserControllerTest.java
@@ -3,9 +3,11 @@ package com.fendy.restful.api.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fendy.restful.api.entity.User;
 import com.fendy.restful.api.model.RegisterUserRequest;
 import com.fendy.restful.api.model.WebResponse;
 import com.fendy.restful.api.repository.UserRepository;
+import com.fendy.restful.api.security.BCrypt;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -60,6 +62,59 @@ class UserControllerTest {
             });
 
             assertEquals("OK", response.getData());
+        });
+    }
+
+    @Test
+    void testRegisterBadRequest() throws Exception {
+
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setUsername("");
+        request.setPassword("");
+        request.setName("");
+
+        mockMvc.perform(
+                post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
+            });
+
+            assertNotNull(response.getErrors());
+        });
+    }
+
+    @Test
+    void testRegisterDuplicateUsername() throws Exception {
+
+        User user = new User();
+        user.setUsername("test_fendy");
+        user.setPassword(BCrypt.hashpw("test_password", BCrypt.gensalt()));
+        user.setName("test_fendy");
+
+        userRepository.save(user);
+
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setUsername("test_fendy");
+        request.setPassword("test_password");
+        request.setName("test_fendy");
+
+        mockMvc.perform(
+                post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
+            });
+
+            assertNotNull(response.getErrors());
         });
     }
 
