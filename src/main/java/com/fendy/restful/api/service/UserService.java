423a5b411900424179008add546c76791267e4e4
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-20 14:04:15 +0700

    Create UserService.java

---
+++ src/main/java/com/fendy/restful/api/service/UserService.java
@@ -0,0 +1,47 @@
+package com.fendy.restful.api.service;
+
+import com.fendy.restful.api.entity.User;
+import com.fendy.restful.api.model.RegisterUserRequest;
+import com.fendy.restful.api.repository.UserRepository;
+import com.fendy.restful.api.security.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private Validator validator;
+
+    public void register(RegisterUserRequest registerUserRequest) {
+
+        // validation request
+        Set<ConstraintViolation<RegisterUserRequest>> constraintViolations = validator.validate(registerUserRequest);
+        if (constraintViolations.size() != 0) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+
+        // check if username existed
+        if (userRepository.existsById(registerUserRequest.getUsername())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already registered");
+        }
+
+        User user = new User();
+        user.setUsername(registerUserRequest.getUsername());
+        user.setPassword(BCrypt.hashpw(registerUserRequest.getPassword(), BCrypt.gensalt()));
+        user.setName(registerUserRequest.getName());
+
+        userRepository.save(user);
+
+    }
+
+}
