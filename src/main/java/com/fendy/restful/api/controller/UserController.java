999e536ddfc8e706d40e29d66d5a9d679d8402d3
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-25 11:04:55 +0700

    Update UserController.java

--- src/main/java/com/fendy/restful/api/controller/UserController.java
+++ src/main/java/com/fendy/restful/api/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.fendy.restful.api.controller;
 
+import com.fendy.restful.api.entity.User;
 import com.fendy.restful.api.model.RegisterUserRequest;
+import com.fendy.restful.api.model.UserResponse;
 import com.fendy.restful.api.model.WebResponse;
 import com.fendy.restful.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +26,18 @@ public class UserController {
     public WebResponse<String> register(@RequestBody RegisterUserRequest registerUserRequest) {
         userService.register(registerUserRequest);
         return WebResponse.<String>builder().data("OK").build();
+    }
+
+    @GetMapping(
+            path = "/api/users/current",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<UserResponse> getUser(User user) {
+        UserResponse userResponse = userService.getUser(user);
+
+        return WebResponse.<UserResponse>builder()
+                .data(userResponse)
+                .build();
     }
 
 }
