f02e48c849d405b3ec8f1c7d9aa89e3843eb46e1
Fendyanto Fendyanto <66929618+antoniofendy@users.noreply.github.com>
2023-07-25 09:47:22 +0700

    Update LoginUserRequest.java

--- src/main/java/com/fendy/restful/api/model/LoginUserRequest.java
+++ src/main/java/com/fendy/restful/api/model/LoginUserRequest.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -14,11 +15,11 @@ import javax.validation.constraints.Size;
 @Builder
 public class LoginUserRequest {
 
-    @NotNull
+    @NotBlank
     @Size(max = 100)
     private String username;
 
-    @NotNull
+    @NotBlank
     @Size(max = 100)
     private String password;
 
